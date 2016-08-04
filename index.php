52d524b87876d1cb6a86da383c98c620de5c350f
Matko Djipalo <matkodjipalo@gmail.com>
2016-08-04 23:34:22 +0200

    - uklonjen višak

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-use Tests\Trikoder\Repository\DummyRepositoryTest;
-use Tests\Trikoder\Model\DummyTest;
-use Trikoder\Repository\DummyRepository;
-use Trikoder\Model\Dummy;
-
 require_once __DIR__.'/config/bootstrap.php';
 
-$x = array(10,2,4);
-$y = array(1 => 2, 0 => 10, 2 => 4);
-d($x == $y);
+// skripta za testiranje tijekom razvoja
